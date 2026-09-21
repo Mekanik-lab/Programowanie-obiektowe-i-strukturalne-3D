c239f63afcd39f3ee326d30730975d9b34e4af1f
Uczen <Uczen@LAPTOP-26CG11IL>
2026-09-21 12:52:12 +0200

    poprawa

--- Programowanie/EventVsBindingMauiApp/MainPage.xaml.cs
+++ Programowanie/EventVsBindingMauiApp/MainPage.xaml.cs
@@ -9,8 +9,21 @@
 
         private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
         {
-            if(sender is Slider slider && labelRotation is not null) 
-                labelRotation.Rotation = slider.Value;
+            /*if (rotationSlider is not null)
+                rotationLabel.Rotation = rotationSlider.Value;*/
+
+            /*
+            Slider slider = sender as Slider;
+            if (slider is not null
+                && rotationLabel is not null)
+                rotationLabel.Rotation = slider.Value;
+
+            */
+
+            if (sender is Slider slider
+                && rotationLabel is not null)
+                rotationLabel.Rotation = slider.Value;
         }
+
     }
 }
