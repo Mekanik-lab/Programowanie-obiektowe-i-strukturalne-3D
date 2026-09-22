e27f95fdd2e1311b237af380de7172cdaf2991b6
Uczen <Uczen@LAPTOP-26CG11IL>
2026-09-22 10:27:54 +0200

    Dodanie i wykonanie zadania, bindowany kalkulator

---
+++ Programowanie/SimpleBindingCalculatorMauiApp/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿namespace SimpleBindingCalculatorMauiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+        private int result;
+
+        public int Result
+        {
+            get
+            {
+                return result;
+
+            }
+
+            set
+            {
+                result = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Result = FirstNumber + SecondNumber;
+        }
+    }
+}
